7c165684bcc50b59abede840e95acdef4dedbeb9
Nikita <niklas.buk@gmail.com>
2021-08-17 18:03:06 +0700

    lesson_1

--- geekbrains/src/App.js
+++ geekbrains/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom'
+
 import './App.css';
+import Message from './message'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+   const [inputText, setInputText] = useState('')
+
+   return (
+      <div className = 'app wrapper'>
+         <h1 className = 'textFormat'>Changing message</h1>
+         <input 
+            value = {inputText} 
+            onChange = { e => setInputText(e.target.value) } 
+            placeholder='Type here to change message' 
+            className = 'input-area'
+         />
+         <p className = 'textFormat pText'>and here is what you wrote</p>
+         <Message textToShow = {inputText} />
+      </div>
+   )
 }
+
+ReactDOM.render(<App />, document.getElementById('root'))
 
 export default App;
